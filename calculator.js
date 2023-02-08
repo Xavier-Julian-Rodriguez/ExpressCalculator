ae188a9a8aeec5d92bc8ab23c141dc7e44a8f3ab
Xavier-Julian-Rodriguez <xavierjulianrod@gmail.com>
2023-02-08 11:49:45 -0500

    added a post request with computation

--- calculator.js
+++ calculator.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res, next) => {
     res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res, next) => {
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+
+    res.send(`The result is: ${result}`);
 })
 
 app.listen(3000, () => {
